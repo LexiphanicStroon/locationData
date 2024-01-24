a4440dc87b2068c848904ac825f7479738026afd
LexiphanicStroon <nate_reeves@outlook.com>
2024-01-24 15:37:37 -0600

    Secure keys, add build script

---
+++ build.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+
+const apiKey = process.env.GOOGLE_MAPS_API_KEY;
+const htmlFilePath = 'src/map.html';
+
+let htmlContent = fs.readFileSync(htmlFilePath, 'utf8');
+htmlContent = htmlContent.replace('MYKEY', apiKey);
+
+fs.writeFileSync(htmlFilePath, htmlContent);
