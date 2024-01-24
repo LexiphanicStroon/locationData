b1d450742d4ee21e8d87cd3b7e357d057fc67587
LexiphanicStroon <nate_reeves@outlook.com>
2024-01-24 16:30:14 -0600

    update key

--- build.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-const apiKey = process.env.GOOGLE_KEY;
-const htmlFilePath = 'src/map.html';
-
-let htmlContent = fs.readFileSync(htmlFilePath, 'utf8');
-htmlContent = htmlContent.replace('MYKEY', apiKey);
-
-fs.writeFileSync(htmlFilePath, htmlContent);
