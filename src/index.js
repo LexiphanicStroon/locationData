af53ef93502c3004013fa01b9139af9897fd0fce
LexiphanicStroon <nate_reeves@outlook.com>
2024-01-25 10:27:16 -0600

    remove user consent

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const mapLink = document.querySelector('#mapLink');
 
-document.getElementById('getLocation').addEventListener('click', function () {
+// document.getElementById('getLocation').addEventListener('click', function () {
   navigator.geolocation.getCurrentPosition((position) => {
     document.cookie = `lat=${position.coords.latitude}; SameSite=Lax; path=/`;
     document.cookie = `lon=${position.coords.longitude}; SameSite=Lax; path=/`;
     mapLink.classList.remove('hidden');
   });
-});
+// });
